44cacc164e45b6c8a8c6c289626d096d936222e0
feizi-discipline <1157379144@qq.com>
2024-12-01 19:57:51 +0800

    add:新优化配置

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    output: 'export',        // 必需：生成静态文件
+    images: {
+      unoptimized: true,    // 必需：GitHub Pages 不支持图片优化
+    },
+    basePath: '/feizi-123.github.io', // 必需：设置基础路径
+    // 添加这个配置来处理 MDX
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  }
 module.exports = nextConfig
