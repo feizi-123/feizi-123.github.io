ad3212f898bd23c847d16062e74d229d0613520e
feizi-123 <strive-123@foxmail.com>
2024-12-08 13:30:24 +0000

    Deploying to gh-pages from @ feizi-123/feizi-123.github.io@5ff59a55171e90ce482fdac3ad9ef313e75a718d 🚀

--- lib/articles.ts
+++
@@ -1,7 +0,0 @@
-import { getAllArticles, getYears } from './mdx'
-
-export const articles = getAllArticles()
-export const sortedArticles = articles.sort(
-  (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-)
-export const years = getYears() 
