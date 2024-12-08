bb730f97c695b25b8056013556cd554d3600e59b
feizi-discipline <1157379144@qq.com>
2024-12-08 21:12:16 +0800

    修复错误

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const repo = 'https://feizi-123.github.io/'
+const repo = 'https://feizi-123.github.io'
 // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
 // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
 let assetPrefix = `/${repo}/`
